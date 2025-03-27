419b387f2baf837a88838f578eddb11680817b48
FernandoCaik <fernandocaikmello@gmail.com>
2025-03-27 15:15:26 -0300

    Adição das perguntas 0 e 1
    Signed-off-by: FernandoCaik <fernandocaikmello@gmail.com>

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,16 @@ public class Main {
         System.out.println("Professor: Brenno Pimenta");
         System.out.println("Bem-vindo ao quiz sobre Carros! Você responderá 15 perguntas de múltipla escolha.");
 
+        Questao[] questoes = new Questao[15];
+        // Definição das perguntas sobre carros
+        questoes[0] = new Questao("1- Qual é a função do motor de um carro?",
+                new String[]{"Gerar eletricidade", "Fornecer tração", "Transformar combustível em energia mecânica", "Resfriar o veículo", "Lubrificar as peças"}, "C");
+        questoes[1] = new Questao("2- O que significa a sigla ABS nos freios de um carro?",
+                new String[]{"Automated Brake System", "Anti-lock Braking System", "Automatic Balance Suspension", "Advanced Braking Security", "Active Brake Support"}, "B");
+
+
+
+
 
     }
 }
